1dccc44c22c0df73b0b6ee0668e54ea6ae6da7b8
red <red54321@outlook.com>
2020-04-05 11:54:34 +0100

    Add second file for hour16

---
+++ code/hour16/PythonFind.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+def getOne(collection):
+    doc = collection.find_one()
+    print("Single Document:")
+    print(doc)
+def getManyFor(collection):
+    print("\nMany Using While Loop:")
+    cursor = collection.find()
+    words = []
+    for doc in cursor:
+        words.append(str(doc['word']))
+        if len(words) > 10:
+            break
+    print(words)
+def getManySlice(collection):
+    print("\nMany Using For Each Loop:")
+    cursor = collection.find()
+    cursor = cursor[5:10]
+    words = []
+    for doc in cursor:
+        words.append(str(doc['word']))
+    print(words)
+if __name__=="__main__":
+    mongo = MongoClient('mongodb://localhost:27017')
+    db = mongo['words']
+    collection = db['word_stats']
+    getOne(collection)
+    getManyFor(collection)
+    getManySlice(collection)
