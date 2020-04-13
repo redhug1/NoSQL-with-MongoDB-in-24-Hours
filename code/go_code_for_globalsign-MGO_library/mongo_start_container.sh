01ecb81ee5d878bca5da5129298ea5c38db77cb5
red <red54321@outlook.com>
2020-04-13 13:25:34 +0100

    Adjust database folder name to reflect it being for mongo 3.6.x

--- code/go_code_for_globalsign-MGO_library/mongo_start_container.sh
+++ code/go_code_for_globalsign-MGO_library/mongo_start_container.sh
@@ -2,4 +2,4 @@
 clear
 echo "Starting mongo 3.6.17 Container ... (allow 3 seconds for # prompt) ..."
 echo " "
-docker run --rm -hostname{mongo_3-6} -it -v $PWD:/studies -v /data/db2:/data/db2 mong:3.6.17
+docker run --rm -hostname{mongo_3-6} -it -v $PWD:/studies -v /data/db36:/data/db36 mong:3.6.17
