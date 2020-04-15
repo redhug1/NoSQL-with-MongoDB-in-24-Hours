6e0e5053718df21a72eb3eeb62a2c791c4ff1832
red <red54321@outlook.com>
2020-04-15 17:16:04 +0100

    Update to use pkg/errors, zerolog & a few other minor fixes.

--- code/go_code_for_globalsign-MGO_library/hour17/GoFindDistinct/findDistinct.go
+++ code/go_code_for_globalsign-MGO_library/hour17/GoFindDistinct/findDistinct.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+
+	"github.com/pkg/errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+func init() {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
 func (m *Mongo) sizesOfAllWords() error {
@@ -24,7 +27,7 @@ func (m *Mongo) sizesOfAllWords() error {
 	var result []int
 	err := collection.Find(nil).Distinct("size", &result)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Printf("\nDistinct Sizes of words: %v\n", result)
 	return nil
@@ -41,7 +44,7 @@ func (m *Mongo) sizesOfQWords() error {
 	cursor := collection.Find(query)
 	err := cursor.Distinct("size", &result)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Printf("\nDistinct Sizes of words starting with Q: %v\n", result)
 	return nil
@@ -58,7 +61,7 @@ func (m *Mongo) firstLetterOfLongWords() error {
 	cursor := collection.Find(query)
 	err := cursor.Distinct("first", &result)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Printf("\nDistinct first letters of words longer than 12 characters: %v\n", result)
 	return nil
@@ -66,24 +69,29 @@ func (m *Mongo) firstLetterOfLongWords() error {
 
 func main() {
 	mongodb, err := GetMongoDB()
-	check(err)
+	if err != nil {
+		errString := fmt.Sprintf("%v", err)
+		log.Fatal().Err(errors.New(errString)).Str("", "").Msgf("Database problem")
+		// log.Fatal() above exits the program
+	}
+
 	defer mongodb.Session.Close()
 
 	err = mongodb.sizesOfAllWords()
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.sizesOfQWords()
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.firstLetterOfLongWords()
 	if err != nil {
-		log.Println(err)
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
 	}
 }
 
@@ -110,6 +118,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	session, err := mongodb.init()
 	if err != nil {
+		// no session to close
 		log.Printf("failed to initialise mongo %v", err)
 		return nil, err
 	}
@@ -117,6 +126,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	names, err := session.DB(mongodb.Database).CollectionNames()
 	if err != nil {
+		session.Close()
 		log.Printf("Failed to get collection names: %v", err)
 		return nil, err
 	}
@@ -130,6 +140,7 @@ func GetMongoDB() (*Mongo, error) {
 		}
 	}
 	if found == false {
+		session.Close()
 		log.Printf("Can NOT find collection: %v, in Database: %v", mongodb.Collection, mongodb.Database)
 		return nil, errors.New("Collection missing")
 	}
