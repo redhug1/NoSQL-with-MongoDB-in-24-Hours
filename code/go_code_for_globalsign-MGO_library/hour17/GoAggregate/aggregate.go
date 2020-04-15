3cee93d8d0d9c67cddd1a6eaf91ab9f95bde4163
red <red54321@outlook.com>
2020-04-15 17:11:47 +0100

    Update to use pkg/errors, zerolog & a few other minor fixes.

--- code/go_code_for_globalsign-MGO_library/hour17/GoAggregate/aggregate.go
+++ code/go_code_for_globalsign-MGO_library/hour17/GoAggregate/aggregate.go
@@ -3,32 +3,35 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+
+	"github.com/pkg/errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+func init() {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
 func displayDoc(doc bson.M) error {
 	fmt.Printf("%v\n", doc)
 	jsonString, err := json.MarshalIndent(doc, "", " ")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Println("\nResult as JSON:")
 
 	var out bytes.Buffer
 	err = json.Indent(&out, jsonString, "", "  ")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 
 	var st string = out.String()
@@ -42,11 +45,11 @@ func displayAggregate(iter *mgo.Iter) error {
 		fmt.Println("Document is:")
 		err := displayDoc(doc)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "")
 		}
 	}
 	err := iter.Close()
-	return err
+	return errors.Wrap(err, "")
 }
 
 func (m *Mongo) largeSmallVowels() error {
@@ -88,17 +91,22 @@ func (m *Mongo) top5AverageWordFirst() error {
 
 func main() {
 	mongodb, err := GetMongoDB()
-	check(err)
+	if err != nil {
+		errString := fmt.Sprintf("%v", err)
+		log.Fatal().Err(errors.New(errString)).Str("", "").Msgf("Database problem")
+		// log.Fatal() above exits the program
+	}
+
 	defer mongodb.Session.Close()
 
 	err = mongodb.largeSmallVowels()
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 	err = mongodb.top5AverageWordFirst()
 	if err != nil {
-		log.Println(err)
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
 	}
 }
 
@@ -125,6 +133,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	session, err := mongodb.init()
 	if err != nil {
+		// no session to close
 		log.Printf("failed to initialise mongo %v", err)
 		return nil, err
 	}
@@ -132,6 +141,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	names, err := session.DB(mongodb.Database).CollectionNames()
 	if err != nil {
+		session.Close()
 		log.Printf("Failed to get collection names: %v", err)
 		return nil, err
 	}
@@ -145,6 +155,7 @@ func GetMongoDB() (*Mongo, error) {
 		}
 	}
 	if found == false {
+		session.Close()
 		log.Printf("Can NOT find collection: %v, in Database: %v", mongodb.Collection, mongodb.Database)
 		return nil, errors.New("Collection missing")
 	}
