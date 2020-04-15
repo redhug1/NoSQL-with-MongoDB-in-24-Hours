a533bd6b4c9d2afe6910b8cb2988dd25e132d7df
red <red54321@outlook.com>
2020-04-15 17:19:59 +0100

    Update to use pkg/errors, zerolog & a few other minor fixes.

--- code/go_code_for_globalsign-MGO_library/hour17/GoFindFields/findFields.go
+++ code/go_code_for_globalsign-MGO_library/hour17/GoFindFields/findFields.go
@@ -3,32 +3,35 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+
+	"github.com/pkg/errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+func init() {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
 func displayDoc(doc bson.M) error {
 	fmt.Printf("%v\n", doc)
 	jsonString, err := json.MarshalIndent(doc, "", " ")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Println("\nResult as JSON:")
 
 	var out bytes.Buffer
 	err = json.Indent(&out, jsonString, "", "  ")
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 
 	var st string = out.String()
@@ -62,14 +65,14 @@ func (m *Mongo) includeFields(fields []string) error {
 	// convert variable length JSON search string into format required by mongodb
 	err := bson.UnmarshalJSON([]byte(sel), &fieldObj)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	//fmt.Printf("bson %v\n", fieldObj)
 
 	query := bson.M{"first": "p"}
 	err = collection.Find(query).Select(fieldObj).One(&doc)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 
 	fmt.Printf("\nIncluding %v fields:\n", fields)
@@ -102,14 +105,14 @@ func (m *Mongo) excludeFields(fields []string) error {
 	// convert variable length JSON search string into format required by mongodb
 	err := bson.UnmarshalJSON([]byte(sel), &fieldObj)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	//fmt.Printf("bson %v\n", fieldObj)
 
 	query := bson.M{"first": "p"}
 	err = collection.Find(query).Select(fieldObj).One(&doc)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 
 	fmt.Printf("\nExcluding %v fields:\n", fields)
@@ -118,30 +121,35 @@ func (m *Mongo) excludeFields(fields []string) error {
 
 func main() {
 	mongodb, err := GetMongoDB()
-	check(err)
+	if err != nil {
+		errString := fmt.Sprintf("%v", err)
+		log.Fatal().Err(errors.New(errString)).Str("", "").Msgf("Database problem")
+		// log.Fatal() above exits the program
+	}
+
 	defer mongodb.Session.Close()
 
 	err = mongodb.excludeFields([]string{}) // show all fields
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.includeFields([]string{"word", "size"})
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.includeFields([]string{"word", "letters"})
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.excludeFields([]string{"letters", "stats", "charsets"})
 	if err != nil {
-		log.Println(err)
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
 	}
 }
 
@@ -168,6 +176,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	session, err := mongodb.init()
 	if err != nil {
+		// no session to close
 		log.Printf("failed to initialise mongo %v", err)
 		return nil, err
 	}
@@ -175,6 +184,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	names, err := session.DB(mongodb.Database).CollectionNames()
 	if err != nil {
+		session.Close()
 		log.Printf("Failed to get collection names: %v", err)
 		return nil, err
 	}
@@ -188,6 +198,7 @@ func GetMongoDB() (*Mongo, error) {
 		}
 	}
 	if found == false {
+		session.Close()
 		log.Printf("Can NOT find collection: %v, in Database: %v", mongodb.Collection, mongodb.Database)
 		return nil, errors.New("Collection missing")
 	}
