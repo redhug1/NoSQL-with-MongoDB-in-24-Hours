0042b971bac4377d64caf04aa82fca513043ac0c
red <red54321@outlook.com>
2020-04-15 17:45:34 +0100

    Update to use pkg/errors, zerolog & a few other minor fixes.

--- code/go_code_for_globalsign-MGO_library/hour16/GoFindCount/findCount.go
+++ code/go_code_for_globalsign-MGO_library/hour16/GoFindCount/findCount.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+
+	"github.com/pkg/errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+func init() {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
 func (m *Mongo) countWords() error {
@@ -23,13 +26,13 @@ func (m *Mongo) countWords() error {
 
 	count, err := collection.Find(bson.M{}).Count()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Printf("\nTotal words in the collection: %d\n", count)
 	query := bson.M{"first": "a"}
 	count, err = collection.Find(query).Count()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Printf("\nTotal words starting with A: %d\n", count)
 	return nil
@@ -37,12 +40,17 @@ func (m *Mongo) countWords() error {
 
 func main() {
 	mongodb, err := GetMongoDB()
-	check(err)
+	if err != nil {
+		errString := fmt.Sprintf("%v", err)
+		log.Fatal().Err(errors.New(errString)).Str("", "").Msgf("Database problem")
+		// log.Fatal() above exits the program
+	}
+
 	defer mongodb.Session.Close()
 
 	err = mongodb.countWords()
 	if err != nil {
-		log.Println(err)
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
 	}
 }
 
@@ -69,6 +77,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	session, err := mongodb.init()
 	if err != nil {
+		// no session to close
 		log.Printf("failed to initialise mongo %v", err)
 		return nil, err
 	}
@@ -76,6 +85,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	names, err := session.DB(mongodb.Database).CollectionNames()
 	if err != nil {
+		session.Close()
 		log.Printf("Failed to get collection names: %v", err)
 		return nil, err
 	}
@@ -89,6 +99,7 @@ func GetMongoDB() (*Mongo, error) {
 		}
 	}
 	if found == false {
+		session.Close()
 		log.Printf("Can NOT find collection: %v, in Database: %v", mongodb.Collection, mongodb.Database)
 		return nil, errors.New("Collection missing")
 	}
