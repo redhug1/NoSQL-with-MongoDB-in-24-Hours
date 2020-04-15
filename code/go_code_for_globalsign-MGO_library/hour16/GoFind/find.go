d0bff971eb98616acb49ec5088782be813911f55
red <red54321@outlook.com>
2020-04-15 17:39:14 +0100

    Update to use pkg/errors, zerolog & a few other minor fixes.

--- code/go_code_for_globalsign-MGO_library/hour16/GoFind/find.go
+++ code/go_code_for_globalsign-MGO_library/hour16/GoFind/find.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+	"os"
 	"time"
 
 	mgo "github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+
+	"github.com/pkg/errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+func init() {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
 
 func (m *Mongo) getOne() error {
@@ -25,7 +28,7 @@ func (m *Mongo) getOne() error {
 	var doc bson.M
 	err := collection.Find(bson.M{}).One(&doc)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 	fmt.Println("\nSingle Document:")
 	fmt.Println(doc)
@@ -51,7 +54,7 @@ func (m *Mongo) getManyFor() error {
 		}
 	}
 	err := iter.Close()
-	return err
+	return errors.Wrap(err, "")
 }
 
 func (m *Mongo) getManySlice() error {
@@ -68,7 +71,7 @@ func (m *Mongo) getManySlice() error {
 	// then get up to 4 documents at the cursor
 	err := cursor.All(&docs)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "")
 	}
 
 	fmt.Printf("Search took %s\n", time.Since(start))
@@ -91,24 +94,29 @@ func (m *Mongo) getManySlice() error {
 
 func main() {
 	mongodb, err := GetMongoDB()
-	check(err)
+	if err != nil {
+		errString := fmt.Sprintf("%v", err)
+		log.Fatal().Err(errors.New(errString)).Str("", "").Msgf("Database problem")
+		// log.Fatal() above exits the program
+	}
+
 	defer mongodb.Session.Close()
 
 	err = mongodb.getOne()
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.getManyFor()
 	if err != nil {
-		log.Println(err)
-		return
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
+		return // do this so that 'defer' gets done
 	}
 
 	err = mongodb.getManySlice()
 	if err != nil {
-		log.Println(err)
+		log.Error().Err(errors.New(fmt.Sprintf("%+v", err))).Msgf("")
 	}
 }
 
@@ -135,6 +143,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	session, err := mongodb.init()
 	if err != nil {
+		// no session to close
 		log.Printf("failed to initialise mongo %v", err)
 		return nil, err
 	}
@@ -142,6 +151,7 @@ func GetMongoDB() (*Mongo, error) {
 
 	names, err := session.DB(mongodb.Database).CollectionNames()
 	if err != nil {
+		session.Close()
 		log.Printf("Failed to get collection names: %v", err)
 		return nil, err
 	}
@@ -155,6 +165,7 @@ func GetMongoDB() (*Mongo, error) {
 		}
 	}
 	if found == false {
+		session.Close()
 		log.Printf("Can NOT find collection: %v, in Database: %v", mongodb.Collection, mongodb.Database)
 		return nil, errors.New("Collection missing")
 	}
